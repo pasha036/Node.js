9548bbfffcb67d090b03aa479eca34f304fa6f12
Serkan UĞUR <pasha036@gmail.com>
2023-02-11 16:25:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const { circleArea, circleCircumference } = require('./circle');
+
+const r = 5;
+
+console.log('Dairenin Alanı: ', circleArea(r));
+console.log('Dairenin Çevresi: ', circleCircumference(r));
